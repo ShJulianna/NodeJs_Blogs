68ad35698749fa1deec4c096595d596d074105a8
ymukha@piesoft.us <ymukha@piesoft.us>
2026-02-23 14:00:19 +0300

    13

--- src/core/settings/settings.ts
+++ src/core/settings/settings.ts
@@ -5,6 +5,8 @@ const mongoURI = process.env.MONGO_URL || "mongodb://0.0.0.0:27017";
 
 export const SETTINGS = {
   PORT: process.env.PORT || 5001,
-  MONGO_URL: process.env.MONGO_URL || "mongodb://localhost:27017/blogs",
+  MONGO_URL:
+    process.env.MONGO_URL ||
+    "mongodb+srv://shj_db:aspo@cluster0.dkqogy6.mongodb.net/?appName=Cluster0",
   DB_NAME: process.env.DB_NAME || "ed-back-lessons-blogs",
 };
--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,34 @@ import { runDB } from "./db/mongo.db";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const bootstrap = async () => {
-  const app = express();
-  setupApp(app);
-  const PORT = SETTINGS.PORT;
-
-  await runDB(SETTINGS.MONGO_URL);
-
-  // app.listen(PORT, () => {
-  //   console.log(`Example app listening on port ${PORT}`);
-  // });
-  return app;
-};
-
-bootstrap();
+// const bootstrap = async () => {
+//   const app = express();
+//   setupApp(app);
+//   const PORT = SETTINGS.PORT;
 //
-// const app = bootstrap();
-// export default app;
+//   await runDB(SETTINGS.MONGO_URL);
+//
+//   app.listen(PORT, () => {
+//    console.log(`Example app listening on port ${PORT}`);
+//   });
+//   return app;
+// };
+//
+// bootstrap();
+
+const app = express();
+
+let dbConnected = false;
+
+async function init() {
+  if (dbConnected) return;
+  await runDB(SETTINGS.MONGO_URL);
+  dbConnected = true;
+}
+
+setupApp(app);
+
+export default async function handler(req, res) {
+  await init();
+  return app(req, res);
+}
