1aa706015f0e12662ab7770264021d6abd14fc51
ymukha@piesoft.us <ymukha@piesoft.us>
2026-02-23 14:51:34 +0300

    14

--- src/index.ts
+++ src/index.ts
@@ -26,24 +26,13 @@ let dbConnected = false;
 
 async function init() {
   if (dbConnected) return;
-  console.log("[init] connecting to DB...");
   await runDB(SETTINGS.MONGO_URL);
   dbConnected = true;
-  console.log("[init] DB connected");
 }
 
-console.log("[index] before setupApp");
 setupApp(app);
-console.log("[index] after setupApp");
-
-// тестовый роут на корень
-app.get("/", (req: Request, res: Response) => {
-  console.log("[GET /] handler called");
-  res.send("API is running");
-});
 
 export default async function handler(req: Request, res: Response) {
-  console.log("[handler] incoming request", req.method, req.url);
   await init();
   return app(req, res);
 }
